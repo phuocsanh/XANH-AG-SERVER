f92ab028771e4b1f1c880e44ecd067bcdb49497e
phuocsanh <phuocsanhtps@gmail.com>
2026-01-12 15:19:14 +0700

    feat: bổ sung trade_name vào SearchProductDto để hỗ trợ lọc

--- src/modules/product/dto/search-product.dto.ts
+++ src/modules/product/dto/search-product.dto.ts
@@ -6,6 +6,7 @@ export class SearchProductDto extends BaseSearchDto {
   @IsOptional() @Type(() => Number) id?: number;
   @IsString() @IsOptional() code?: string;
   @IsString() @IsOptional() name?: string;
+  @IsString() @IsOptional() trade_name?: string;
   
   @IsNumber() @IsOptional() @Type(() => Number) unit_id?: number;
   @IsNumber() @IsOptional() @Type(() => Number) type_id?: number;
